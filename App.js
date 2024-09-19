84158050cff57397aac2f3c6f007a35eecdfacb4
jfsaenzr <jocksaenz27@hotmail.com>
2024-09-19 15:19:19 -0500

    Save

--- App.js
+++ App.js
@@ -1,65 +1,65 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { BarChart, LineChart } from 'react-native-graph-kit';
+import React from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
-const data = {
-  xAxis: {
-    labels: [
-      'Alice',
-      'Bob',
-      'Charlie',
-      'Liam',
-      'Mia',
-      'Nora',
-      'Oliver',
-      'Penelope',
-      'Quinn',
-      'Ryan',
-      'Sophia'
-    ]
-  },
-  yAxis: {
-    datasets: [120, 350, 400, 70, 87, 655, 10, 20, 400, 70, 87]
-  }
-};
+import {
+  BarChart,
+  LineChart,
+  PieChart,
+  PopulationPyramid,
+} from "react-native-gifted-charts";
 
-const App = () => (
-  <SafeAreaView style={styles.screen}>
-    <View style={styles.chartContainer}>
-      <BarChart
-        chartData={data}
-        barWidth={30}
-        barGap={70}
-        horizontalGridLineColor={'grey'}
-        chartHeight={300}
-        yAxisMax={1000}
-      />
-    </View>
-    <View style={styles.chartContainer}>
-      <LineChart yAxisMax={1000} chartData={data} xAxisLength={70} initialDistance={30} />
-    </View>
-  </SafeAreaView>
-);
+const data = [{ value: 50 }, { value: 80 }, { value: 90 }, { value: 70 }];
+const barData = [
+  { value: 250, label: "M" },
+  { value: 500, label: "T", frontColor: "#177AD5" },
+  { value: 745, label: "W", frontColor: "#177AD5" },
+  { value: 320, label: "T" },
+  { value: 600, label: "F", frontColor: "#177AD5" },
+  { value: 256, label: "S" },
+  { value: 300, label: "S" },
+];
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.screen}>
+      <View style={styles.chartContainer}>
+        <BarChart
+          barWidth={22}
+          noOfSections={3}
+          barBorderRadius={4}
+          frontColor="lightgray"
+          data={barData}
+          yAxisThickness={0}
+          xAxisThickness={0}
+        />
+      </View>
+      <View style={styles.chartContainer}>
+        <LineChart data={data} />
+      </View>
+      <View style={styles.chartContainer}>
+        <PieChart data={data} />
+      </View>
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1
+    flex: 1,
   },
   chartContainer: {
     flex: 1,
     borderRadius: 10,
     borderWidth: 0.5,
-    borderColor: 'lightgrey',
+    borderColor: "lightgrey",
     padding: 10,
     margin: 10,
-    shadowColor: 'lightgrey',
+    shadowColor: "lightgrey",
     shadowOpacity: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     shadowOffset: {
       height: 6,
-      width: 5
-    }
-  }
+      width: 5,
+    },
+  },
 });
-
-export default App;
